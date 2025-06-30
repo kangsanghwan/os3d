feb143b4ce1cc704dc8485e683c503f3cd9bc014
kangsanghwan <47845232+kangsanghwan@users.noreply.github.com>
2025-06-30 16:59:34 +0900

    작업완료

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import fs from 'fs'
 import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import handlebars from 'vite-plugin-handlebars'
+import chokidar from 'chokidar'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
@@ -84,6 +85,21 @@ export default defineConfig(({ mode }) => {
           pages: pageMetaList
         }
       }),
+      {
+        name: 'watch-handlebars-partials',
+        configureServer(server) {
+          const partialsDir = path.resolve(__dirname, 'src/components')
+          const watcher = chokidar.watch(partialsDir, {
+            ignoreInitial: true
+          })
+          watcher.on('all', () => {
+            // 파일 변경시 강제 reload
+            server.ws.send({
+              type: 'full-reload'
+            })
+          })
+        }
+      },
       {
         name: 'cleanup-html',
         closeBundle() {
